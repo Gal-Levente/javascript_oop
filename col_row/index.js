3d5509f7c36da2af1e54b8df6260a023677e7451
Gal Levente <gal.levente_2022b@bolyai-szakkozep.hu>
2026-02-13 08:42:41 +0100

    02.13_ora

--- col_row/index.js
+++ col_row/index.js
@@ -6,21 +6,79 @@ import data from './data.json' with {type:'json'}
 import { Manager } from './manager.js';
 import { Table } from './table.js';
 
+// COLSPAN
 /** @type {Manager} */
 const manager = new Manager();
 
-// COLSPAN
-/** @type {Table} */
-new Table(data.colspanHeaderArray, manager);
+const renderColspanBody = (tbody, elem) => {
+    const tr = document.createElement('tr');
+    tbody.appendChild(tr);
 
+    const td1 = document.createElement('td');
+    td1.innerText = elem.neve;
+    tr.appendChild(td1);
+
+    const td2 = document.createElement('td');
+    td2.innerText = elem.kor;
+    tr.appendChild(td2);
+
+    const td3 = document.createElement('td');
+    td3.innerText = elem.szerelme1;
+    tr.appendChild(td3);
+
+    if(elem.szerelme2) {
+        const td4 = document.createElement('td');
+        td4.innerText = elem.szerelme2;
+        tr.appendChild(td4);
+    } else {
+        td3.colSpan = 2;    
+    }
+}
+
+/** @type {Table} */
+const table_col = new Table(data.colspanHeaderArray, manager);
+table_col.setAppendRow(renderColspanBody);
 for(const element of data.colspanDataArr) {
     manager.addElement(element);
 }
 
 // ROWSPAN
-/** @type {Table} */
-new Table(data.rowspanHeaderArray, manager);
+/** @type {Manager} */
+const manager2 = new Manager();
 
+const renderRowspanBody = (tbody, elem) => {
+    const tr = document.createElement('tr');
+    tbody.appendChild(tr);
+
+    const td1 = document.createElement('td');
+    td1.innerText = elem.nemzet;
+    tr.appendChild(td1);
+
+    const td2 = document.createElement('td');
+    td2.innerText = elem.szerzo;
+    tr.appendChild(td2);
+
+    const td3 = document.createElement('td');
+    td3.innerText = elem.mu;
+    tr.appendChild(td3);
+
+    if(elem.szerzo2) {
+        td1.rowSpan = 2;
+        const tr2 = document.createElement('tr');
+        tbody.appendChild(tr2);
+        const td4 = document.createElement('td');
+        td4.innerText = elem.szerzo2;
+        tr.appendChild(td4);
+        tr2.appendChild(td4);
+        const td5 = document.createElement('td');
+        td5.innerText = elem.mu2;
+        tr2.appendChild(td5);
+    }
+}
+
+/** @type {Table} */
+const table_row = new Table(data.rowspanHeaderArray, manager2);
+table_row.setAppendRow(renderRowspanBody);
 for(const element of data.rowspanTableArray) {
-    manager.addElement(element);
+    manager2.addElement(element);
 }
--- col_row/table.js
+++ col_row/table.js
@@ -1,6 +1,12 @@
 /**
  * @import {FormFieldType, HeaderArrayType, ColspanType, RowspanType} from "./functions.js"
  */
+/**
+ * @callback TableCallback
+ * @param {HTMLTableSectionElement} tbody
+ * @param {ColspanType | RowspanType} elem
+ * @returns {void}
+ */
 import { Manager } from "./manager.js";
 
 class Table {
@@ -13,6 +19,8 @@ class Table {
      * @param {Manager} manager 
      */
     constructor(headerArray, manager) {
+        this.#manager = manager;
+
         const table = document.createElement('table');
         document.body.appendChild(table);
 
@@ -32,35 +40,18 @@ class Table {
 
         const tbody = document.createElement('tbody');
         table.appendChild(tbody);
-
         this.#tbody = tbody;
-        this.#manager = manager;
-
-        /** @param {ColspanType | RowspanType} element */
-        this.#manager.addCallback = (element) => {
-            const tr2 = document.createElement('tr');
-            const tr = document.createElement('tr');
-            for (const key in element) {
-                const td2 = document.createElement('td');
-                const td = document.createElement('td');
-                td.innerText = element[key];
-                if (key === 'szerelme1' && !element.szerelme2) {
-                    td.colSpan = 2;
-                }
-                if(key === 'szerzo2' || key === 'mu2') {
-                    td2.innerText = element[key];
-                    tr2.appendChild(td2);
-                    continue;
-                }
-                if(element.mu2 && element.szerzo2 && key === 'nemzet') {
-                    td.rowSpan = 2;
-                }
-                tr.appendChild(td);
-            }
-            this.#tbody.appendChild(tr);
-            this.#tbody.appendChild(tr2);
+        
         };
-    }
+        /**
+         * @param {TableCallback} tableCallback
+         */
+        setAppendRow(tableCallback) {
+            this.#manager.addCallback = (element) => {
+                tableCallback(this.#tbody, element);
+            }
+        }
 }
+
 
 export { Table };
